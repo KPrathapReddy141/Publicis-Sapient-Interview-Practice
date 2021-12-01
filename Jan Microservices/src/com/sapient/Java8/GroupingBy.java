28d16e29a7a3a3ee189a021dd90ffffeb4861509
saikambh <saikambh@WKWIN1407684.global.publicisgroupe.net>
2021-12-01 15:32:42 +0530

    commit

--- Jan Microservices/src/com/sapient/Java8/GroupingBy.java
+++ Jan Microservices/src/com/sapient/Java8/GroupingBy.java
@@ -1,14 +1,15 @@
 package com.sapient.Java8;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 public class GroupingBy {
 
 	public static void main(String[] args) {
 
-		Map<Character, Long> map = Arrays.asList('s', 's').stream().collect(Collectors.groupingBy(s->s, 
-				Collectors.counting()));
+		Map<String, List<String>> map = Arrays.asList("s", "ss","ss").stream().collect(Collectors.groupingBy(s->s, 
+				Collectors.toList()));
 		System.out.println(map);
 	}
 }
